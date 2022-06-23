9e21d51d36756abf30e4613c080ff94377d1038b
isw120 <row120@hotmail.fr>
2022-06-23 16:28:52 +0200

    Minor fix

--- Epic_Events/Epic_Events/urls.py
+++ Epic_Events/Epic_Events/urls.py
@@ -20,7 +20,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import ClientView, ContractView, EventView, ReadOnlyClient, ReadOnlyContract, ReadOnlyEvent
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('ReadOnlyClient/', ReadOnlyClient.as_view()),
